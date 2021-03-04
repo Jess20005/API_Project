4b186ce89b43b3af40abd6a9802d01bc80de26d2
Jason Jones <jason.e.jones@gmail.com>
2021-03-03 22:00:16 -0500

    Jokes are posted to Slack.  What more could you possibly want? :-)

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,6 +7,14 @@ fetch("https://icanhazdadjoke.com/", {
 	return response.json();
 }).then((data) => {
 	console.log('RESPONSE ', data)
+	//
+	// lets post this joke to slack shall we...
+	//
+	var url="https://hooks.slack.com/services/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";
+    var payload={"text": data.joke};
+    $.post(url,JSON.stringify(payload),function(data){
+        console.log("I just slacked this joke "+JSON.stringify(payload));
+    })
 })
 .catch(err => {
 	console.error(err);
