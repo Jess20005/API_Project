63ea6d3270aecfd3825736ee3ed4113178d92cdf
Jason Jones <jason.e.jones@gmail.com>
2021-03-04 22:58:30 -0500

    fixed broken slack API link

--- assets/script/background.js
+++ assets/script/background.js
@@ -1,8 +1,7 @@
 const nasaUrl='https://api.nasa.gov/planetary/apod?api_key=xO8d0RCLIdY6C1PoXiXLhIuaUiKvgaZ0N6BrsflD';
 
 var bgImage = document.getElementById('bgimage');
-console.log (bgImage);
-console.log (bgImage.src);
+// default image
 bgImage.src="https://www.fillmurray.com/1920/1080";
 
 function getApi(requestUrl) {
--- assets/script/script.js
+++ assets/script/script.js
@@ -13,21 +13,23 @@ jokeBtn.addEventListener("click", function () {
     })
     .then((data) => {
     //   console.log("RESPONSE", data.joke);
-      jokeText.innerHTML = data.joke;
-      
-    });
+    //
+    jokeText.innerHTML = data.joke;  // <---- instead of putting this in jokeText -what if you just stuck it right on the banner hero page?
     
+    //
+    // code to post jokes to slack
+    // Weird - the OLD API link was https://hooks.slack.com/services/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
+    // 3-4-21 jason.e.jones@gmail.com went back to slack, there were no webhook APIs, so I created a NEW one
+    // and tested the joke posting and it worked?  Not sure why or how the webhook was deleted at slack that's really weird
+    //
+    
+    var url = "https://hooks.slack.com/services/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";
+    var payload = { text: data.joke };
+    $.post(url, JSON.stringify(payload), function (data) {
+      console.log("I just slacked this joke " + JSON.stringify(payload));
+    });
+    })
+    .catch((err) => {
+      console.error(err);       
+    });
 })
-
-
-//
-//     var url =
-//       "https://hooks.slack.com/services/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";
-//     var payload = { text: data.joke };
-//     $.post(url, JSON.stringify(payload), function (data) {
-//       console.log("I just slacked this joke " + JSON.stringify(payload));
-//     });
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   
