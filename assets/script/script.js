9abe21b4e0ed9e5efd3afb472ec0ad1150371689
Jessica <simply.me05@hotmail.com>
2021-03-04 20:36:21 -0500

    jokes display to screen, update to script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,33 @@
-fetch("https://icanhazdadjoke.com/", {
-	headers: {
-		'Accept': 'application/json'
-	}
-})
-.then(response => {
-	return response.json();
-}).then((data) => {
-<<<<<<< HEAD
-	console.log('RESPONSE', data)
-=======
-	console.log('RESPONSE ', data)
-	//
-	// lets post this joke to slack shall we...
-	//
-	var url="https://hooks.slack.com/services/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";
-    var payload={"text": data.joke};
-    $.post(url,JSON.stringify(payload),function(data){
-        console.log("I just slacked this joke "+JSON.stringify(payload));
+var jokeBtn = document.getElementById("jokeBtn");
+var jokeText = document.getElementById("jokeText");
+
+jokeBtn.addEventListener("click", function () {
+  
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
     })
->>>>>>> 2c79bf0e742ef3eafa20431fe5f295de37c3739d
-})
-.catch(err => {
-	console.error(err);
-});
+    .then((data) => {
+    //   console.log("RESPONSE", data.joke);
+      jokeText.innerHTML = data.joke;
       
+    });
+    
+})
+
+
+//
+//     var url =
+//       "https://hooks.slack.com/services/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";
+//     var payload = { text: data.joke };
+//     $.post(url, JSON.stringify(payload), function (data) {
+//       console.log("I just slacked this joke " + JSON.stringify(payload));
+//     });
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   
