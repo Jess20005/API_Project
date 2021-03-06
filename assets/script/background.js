6ed2edde3fd16f0e3ce40b79735e616381ee4e91
Jason Jones <jason.e.jones@gmail.com>
2021-03-06 12:59:04 -0500

    checkpoint - refresh from main

--- assets/script/background.js
+++ assets/script/background.js
@@ -7,13 +7,12 @@ bgImage.src="https://www.fillmurray.com/1920/1080";
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
-      //  Conditional for the the response.status.
       return response.json();
     })
     .then(function (data) {
-      // Make sure to look at the response in the console and read how 404 response is structured.
       bgImage.src=data.url;
-      bgImage.alt="NASA Image of the Day";
+      bgImage.alt=data.title;
+      return (data);   
     });
 }
 getApi(nasaUrl);
